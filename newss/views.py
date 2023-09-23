6f36bab7d418317e35964dc70bb38da23687c83b
hermankrasnozhenov <nektogun@gmail.com>
2023-09-23 19:39:56 +0200

    initial statis confurations

---
+++ newss/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def category(request):
+    return render(request, 'category-masonry.html')
